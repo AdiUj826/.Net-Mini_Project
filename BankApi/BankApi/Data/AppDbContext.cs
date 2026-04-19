acbc9042951e38063a5ec6460764dc9ff08260c8
aditya <adiujgare88@gmail.com>
2026-04-19 09:22:27 +0530

    BankApi project upload

---
+++ BankApi/BankApi/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using BankApi.Models;
+
+namespace BankApi.Data  
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BankAccount> Accounts { get; set; }
+    }
+}
